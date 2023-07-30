1cc10bdce5619bdc012dc6a5f14ad6e776f2baac
ygryPs <70763071+ygryPs@users.noreply.github.com>
2023-07-30 11:25:15 +0300

    Handle accelerometer registering

--- app/src/main/java/com/ygryps/extendedscreen/AutoScrollService.kt
+++ app/src/main/java/com/ygryps/extendedscreen/AutoScrollService.kt
@@ -1,7 +1,12 @@
 package com.ygryps.extendedscreen
 
 import android.app.Service
+import android.content.Context
 import android.content.Intent
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.IBinder
 import android.util.Log
 
@@ -11,13 +16,33 @@ class AutoScrollService : Service() {
         return null
     }
 
+    private val sensorEventListener = object : SensorEventListener {
+        override fun onSensorChanged(event: SensorEvent) {
+            // This method is called whenever the sensor values change
+            // Process the sensor data here
+        }
+
+        override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {
+            // This method is called when the accuracy of the sensor changes
+        }
+    }
+
     override fun onCreate() {
         Log.d("AutoScrollService", "$this created")
         super.onCreate()
+
+        val sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        val accelerometerSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+        sensorManager.registerListener(
+            sensorEventListener, accelerometerSensor, SensorManager.SENSOR_DELAY_NORMAL
+        ) // TODO: choose delay
     }
 
     override fun onDestroy() {
         Log.d("AutoScrollService", "$this destroyed")
         super.onDestroy()
+
+        val sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        sensorManager.unregisterListener(sensorEventListener)
     }
 }
