f554b4a50da3214a8f1d6211e0fb93a1a0d8170d
ygryPs <70763071+ygryPs@users.noreply.github.com>
2023-07-30 12:01:57 +0300

    Implement simple numerical integration

--- app/src/main/java/com/ygryps/extendedscreen/AutoScrollService.kt
+++ app/src/main/java/com/ygryps/extendedscreen/AutoScrollService.kt
@@ -17,9 +17,34 @@ class AutoScrollService : Service() {
     }
 
     private val sensorEventListener = object : SensorEventListener {
+        private var velocityX = 0f
+        private var velocityY = 0f
+        private var displacementX = 0f
+        private var displacementY = 0f
+        private val SECS_PER_NS = 1.0f / 1_000_000_000.0f
+        var lastTimestamp: Long = 0 // remember to initialize when registering listener
+
         override fun onSensorChanged(event: SensorEvent) {
-            // This method is called whenever the sensor values change
-            // Process the sensor data here
+            if (event.sensor.type == Sensor.TYPE_LINEAR_ACCELERATION) {
+                // Get the acceleration values along the X and Y axes
+                val accelerationX = event.values[0]
+                val accelerationY = event.values[1]
+
+                // Get the time elapsed since the last sensor update (in seconds)
+                val deltaTime = (event.timestamp - lastTimestamp) * SECS_PER_NS
+                lastTimestamp = event.timestamp
+
+                // Update velocity based on the acceleration
+                velocityX += accelerationX * deltaTime
+                velocityY += accelerationY * deltaTime
+
+                // Update displacement based on the velocity
+                displacementX += velocityX * deltaTime
+                displacementY += velocityY * deltaTime
+
+                // Use the displacement values for your application logic
+                // For example, update the position of a view, etc.
+            }
         }
 
         override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {
@@ -32,7 +57,8 @@ class AutoScrollService : Service() {
         super.onCreate()
 
         val sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
-        val accelerometerSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+        val accelerometerSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION)
+        sensorEventListener.lastTimestamp = System.nanoTime()
         sensorManager.registerListener(
             sensorEventListener, accelerometerSensor, SensorManager.SENSOR_DELAY_NORMAL
         ) // TODO: choose delay
